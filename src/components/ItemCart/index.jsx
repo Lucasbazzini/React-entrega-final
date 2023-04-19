85e7032520ce7cab971578d7aba2fbc03bd5ea4d
Lucas Bazzini <lbazzini488@gmail.com>
2023-04-18 23:05:08 -0300

    hash probado

--- src/components/ItemCart/index.jsx
+++ src/components/ItemCart/index.jsx
@@ -1,22 +1,22 @@
-// import 'item-cart.css'
-import useCartContext from '../../context/CartContext'
+import React, { useContext } from 'react'
+import CartContext from '../../context/CartContext'
 
 const ItemCart = ({product}) => {
 
-    const {quitarProcucto} = useCartContext();
+    const { quitarProducto } = useContext(CartContext);
+    console.log(quitarProducto);
     
-  return (
-    <div className='itemCartContainer'>
-        <img src={product.image} alt={product.title} />
-        <div>
-            <h1>{product.title}</h1>
-            <p>Cantidad seleccionado : {product.quantity}</p>
-            <p>Precio por unidad: {product.price}</p>
-            <p>Subtotal: ${product.quantity * product.price}</p>
-            <button onClick={() => quitarProcucto(product.id)}>Eliminar del carrito </button>
+    return (
+        <div className='itemCartContainer'>
+            <img src={product.image} alt={product.title} />
+            <div>
+                <h1>{product.title}</h1>
+                <p>Cantidad seleccionado : {product.quantity}</p>
+                <p>Precio por unidad: {product.price}</p>
+                <p>Subtotal: ${product.quantity * product.price}</p>
+                <button onClick={() => quitarProducto(product.id)}>Eliminar del carrito </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
-
 export default ItemCart
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,59 +5,61 @@ const CartContext = React.createContext([])
 export const useCartContext = () => useContext(CartContext)
 
 const CartProvider = ({ children }) => {
-	const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  //AGREGAR ALGUN PRODUCTO AL CARRITO
-  const addProduct = (item, quantity) => {
-		if (dentroCarrito(item.id)) {
-			setCart(
-				cart.map((product) => {
-					return product.id === item.id
-						? { ...product, quantity: product.quantity + quantity }
-						: product;
-				}),
-			);
-		} else {
-			setCart([...cart, { ...item, quantity }]);
-		}
-	};
+    //AGREGAR ALGUN PRODUCTO AL CARRITO
+    const addProduct = (item, quantity) => {
+        if (dentroCarrito(item.id)) {
+            setCart(
+                cart.map((product) => {
+                    return product.id === item.id
+                        ? { ...product, quantity: product.quantity + quantity }
+                        : product;
+                }),
+            );
+        } else {
+            setCart([...cart, { ...item, quantity }]);
+        }
+    };
+
+    //PRECIO TOTAL DEL CARRITO
+    const totalPrice = () => {
+        return cart.reduce((prev, act) => prev + act.quantity * act.price, 0);
+    };
+
+    //CANTIDAD TOTAL DE PRODUCTOS
+    const totalProducts = () =>
+        cart.reduce(
+            (acumulador, productoActual) => acumulador + productoActual.quantity,
+            0,
+        );
+
+    
+    // FUNCION LIMPIAR CARRITO DE COMPRAS
+    const limpiarCarrito = () => setCart([])
+
+    // FUNCION SABER SI ESTA EN EL CARRITO O NO
+    const dentroCarrito = (id) =>
+        cart.find((product) => product.id === id) ? true : false;
+
+    //BORRAR ALGUN PRODUCTO DEL CARRITO
+    const quitarProducto = (id) => setCart(cart.filter(producto => producto.id !== id));
 
 
-  const totalPrice = () => {
-		return cart.reduce((prev, act) => prev + act.quantity * act.price, 0);
-	};
-
-  const totalProducts = () =>
-		cart.reduce(
-			(acumulador, productoActual) => acumulador + productoActual.quantity,
-			0,
-		);
-
-  
-  // FUNCION LIMPIAR CARRITO DE COMPRAS
-  const limpiarCarrito = () => setCart([])
-
-  // FUNCION SABER SI ESTA EN EL CARRITO O NO
-  const dentroCarrito = (id) =>
-		cart.find((product) => product.id === id) ? true : false;
-
-  //BORRAR ALGUN PRODUCTO DEL CARRITO
-  const quitarProcucto = (id) => setCart(cart.filter(producto => producto.id !== id))
-
-  return (
-		<CartContext.Provider
-			value={{
-				limpiarCarrito,
-				dentroCarrito,
-				quitarProcucto,
-				addProduct,
-				totalPrice,
-				totalProducts,
-				cart,
-			}}
-		>
-			{children}
-		</CartContext.Provider>
+    return (
+        <CartContext.Provider
+            value={{
+                limpiarCarrito,
+                dentroCarrito,
+                quitarProducto,
+                addProduct,
+                totalPrice,
+                totalProducts,
+                cart,
+            }}
+        >
+            {children}
+        </CartContext.Provider>
 	);
 };
 
